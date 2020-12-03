63496e09df492c1ad65170a13883f374b6da1b1b
Jan Napieralski <janek2000-2000@wp.pl>
2020-12-03 15:17:14 +0100

    Init commit

---
+++ src/main/java/Sets.java
@@ -0,0 +1,35 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.Scanner;
+
+public abstract class Sets {
+
+    public static void getSets() throws IOException {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        Document doc = Jsoup.connect("https://gatherer.wizards.com/Pages/Default.aspx").get();
+        Elements setTags = doc.select("select[id=ctl00_ctl00_MainContent_Content_SearchControls_setAddText] > option[value]:not([value=\"\"])");
+        for(Element option : setTags){
+            System.out.println(option.html());
+        }
+        System.out.println("-------------------------------------------");
+        System.out.println("Wpisz dokładną nazwę interesującego Cię zestawu : ");
+        Scanner scan = new Scanner(System.in);
+        String chosenSet = scan.next();
+        getSet(chosenSet);
+    }
+
+    public static void getSet(String setName){
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));
+        System.out.println("jej");
+    }
+
+
+
+
+
+
+}
