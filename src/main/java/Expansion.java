e31364b6e1c71caff42e8a0a05f25bd6a33aaa7f
Jan Napieralski <janek2000-2000@wp.pl>
2020-12-07 12:32:06 +0100

    succesfull fetch of cards from first page of site

--- src/main/java/Expansion.java
+++ src/main/java/Expansion.java
@@ -6,8 +6,6 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Expansion {
 
@@ -28,33 +26,37 @@ public class Expansion {
         System.out.println("--------------------------------------------------------");
     }
 
-    public Expansion(String expansionName) throws InterruptedException, IOException {
+    public Expansion(String expansionName) throws IOException {
 
         if(!legalSets.contains(expansionName)){
             throw new IllegalArgumentException("Wpisano złą nazwę zestawu, pomijanie dodawania zestawu \""+expansionName+"\"");
         }
 
-        this.expansionName = expansionName;
-        Document doc = Jsoup.connect("https://gatherer.wizards.com/Pages/Search/Default.aspx?output=compact&set=[\""+this.expansionName+"\"]").get();
-        int numberOfCards = Integer.parseInt(doc.select("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContentHeader_searchTermDisplay").html().split("\\(")[1].replace("(","").replace(")",""));
-
-        //SKORZYSTAJ Z API SCRYFALLU ZAMIAST STRONY MTG !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         System.out.println(new String(new char[50]).replace("\0", "\r\n"));
 
-        Document doc1 = Jsoup.connect("https://gatherer.wizards.com/Pages/Search/Default.aspx?page=0&output=compact&set=[%22Amonkhet%22]").get();
-        int countCards = 0; // potem to będzie po prostu zmienna i w pętli
+        this.expansionName = expansionName;
+
+        Document doc1 = Jsoup.connect("https://gatherer.wizards.com/Pages/Search/Default.aspx?page=0&output=compact&set=["+this.expansionName+"]").get();
+        int numberOfCards = Integer.parseInt(doc1.select("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContentHeader_searchTermDisplay").html().split("\\(")[1].replace("(","").replace(")",""));
+
+
+        System.out.println("Importowanie kart z dodatku "+this.expansionName+" trwa...");
+        for(int i = 1; i<numberOfCards+1;i++){
+            System.out.print(i+"/"+numberOfCards+"\r");
+            System.out.println(getCard(doc1,expansionName,i));
+
+
+        }
+    }
+    public static StringBuilder getCard(Document doc1, String expansionName, int i) throws IOException {
         Elements cardsCompact = doc1.select(".cardItem");
-
         StringBuilder cardString = new StringBuilder();
-        String cardName=cardsCompact.get(0).select(".name.top>a").html();
-
-        String cardNameUrl = cardName.replace(' ','-');
-
+        String cardName=cardsCompact.get(i).select(".name.top>a").html();
         cardString.append(cardName).append(" ");
 
 
 
-        Elements manaCosts =cardsCompact.get(0).select(".mana.top > img");
+        Elements manaCosts =cardsCompact.get(i).select(".mana.top > img");
         int cmc = 0;
         for(Element img : manaCosts){
             if(!StringUtil.isNumeric(img.attr("alt"))){
@@ -68,44 +70,55 @@ public class Expansion {
         }
         cardString.append(cmc);
 
-        if(cardsCompact.get(0).select(".type.top").html().contains(" ")){
-            cardString.append(cardsCompact.get(0).select(".type.top").html().split("\\s+")[0]);
+        if(cardsCompact.get(i).select(".type.top").html().contains(" ")){
+            cardString.append(cardsCompact.get(i).select(".type.top").html().split("\\s+")[0]);
         }
         else{
-            cardString.append(cardsCompact.get(0).select(".type.top").html());
+            cardString.append(cardsCompact.get(i).select(".type.top").html());
         }
-        Elements stats = cardsCompact.get(0).select(".numerical.top");
+        Elements stats = cardsCompact.get(i).select(".numerical.top");
         cardString.append(stats.get(0).html());
         cardString.append(stats.get(1).html());
 
 
-        String href = cardsCompact.get(0).select(".name.top>a").attr("abs:href");
+        String href = cardsCompact.get(i).select(".name.top>a").attr("abs:href");
         Document cardDataDetailed = Jsoup.connect(href).get();
         Elements rarityElements = cardDataDetailed.select("div.value>span");
         cardString.append(" ").append(rarityElements.get(1).html());
+        //dodanie artysty i numeru karty
         cardString.append(" ").append(cardDataDetailed.select("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_CardNumberValue").html());
         cardString.append(" ").append(cardDataDetailed.select("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_ArtistCredit>a").html());
 
+        String cardNameUrl;
+        String cardNameToTest = cardDataDetailed.select("#ctl00_ctl00_ctl00_MainContent_SubContent_SubContentHeader_subtitleDisplay").html();
+        if (cardNameToTest.contains("//")) {
+            cardNameUrl = cardNameToTest.replace(" ","").replace("//","-");
+        }
+        else{
+            cardNameUrl = cardNameToTest.replace(' ','-').replace('\'','-').replace("//","-");
+        }
 
 
         //pobieranie ceny
+
         Document cardMarket = Jsoup.connect("https://www.cardmarket.com/en/Magic/Products/Singles/"+expansionName+"/"+cardNameUrl).get();
-        String price = cardMarket.select(".col-6.col-xl-7").get(4).html().replace("€", "").replace(",",".");
+        Elements dd = cardMarket.select(".col-6");
+        int indexOfPriceElement = 0;
+        for(Element single_dd : dd){
+            if(single_dd.text().contains("From")){
+                indexOfPriceElement = dd.indexOf(single_dd)+1;
+                break;
+            }
+        }
+        String price = dd.get(indexOfPriceElement).html().replace("€", "").replace(",",".");
         cardString.append(" ").append(price);
 
 
         //pobieranie obrazka
         String imageUrl = cardMarket.select("div.image.card-image.is-magic>img.is-front").get(1).attr("src");
         cardString.append(" ").append(imageUrl);
-        System.out.println(cardString);
 
-
-        /*
-        System.out.println("Importowanie kart z dodatku "+this.expansionName+" trwa...");
-        for(int i = 1; i<numberOfCards+1;i++){
-            System.out.print(i+"/"+numberOfCards+"\r");
-            Thread.sleep(100);
-        }*/
+        return cardString;
     }
 
 
