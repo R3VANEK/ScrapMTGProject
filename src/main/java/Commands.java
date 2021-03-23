1ea5b0acc8ec6c45d83c8f0e457f9f3c5c05f75e
REVANPL <janek2000-2000@wp.pl>
2021-03-23 11:29:07 +0100

    improved sql init

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -8,54 +8,68 @@
 public interface Commands {
 
 
-    String sqlCreateDB = "CREATE DATABASE mtg";
+    String sqlInit = """
+
+                CREATE DATABASE mtg;
+                USE mtg;
+                
+                START TRANSACTION;
+
+                CREATE TABLE cards(
+                    id_card INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                    card_name VARCHAR(60) NOT NULL UNIQUE,
+                    card_image VARCHAR(150) NOT NULL UNIQUE,
+                    mana_cost VARCHAR(9) NOT NULL,
+                    converted_mana_cost INTEGER NOT NULL,
+                    card_number INTEGER NOT NULL,
+                    card_type VARCHAR(40) NOT NULL,
+                    rarity VARCHAR(11) NOT NULL,
+                    power VARCHAR(4) NOT NULL,
+                    toughness VARCHAR(4) NOT NULL,
+                    uploaded_date DATETIME DEFAULT CURRENT_TIMESTAMP
+                );
 
 
-    String sqlCards = "CREATE TABLE cards"+
-            "(id_card INTEGER NOT NULL AUTO_INCREMENT,"+
-            "card_name VARCHAR(60) NOT NULL UNIQUE,"+
-            "card_image VARCHAR(150) NOT NULL UNIQUE,"+
-            "mana_cost VARCHAR(9) NOT NULL,"+
-            "converted_mana_cost INTEGER NOT NULL,"+
-            "card_number INTEGER NOT NULL,"+
-            "card_type VARCHAR(40) NOT NULL,"+
-            "rarity VARCHAR(11) NOT NULL,"+
-            "power VARCHAR(4) NOT NULL,"+
-            "toughness VARCHAR(4) NOT NULL,"+
-            "PRIMARY KEY ( id_card ))";
-
-    String sqlExpansion = "CREATE TABLE expansions"+
-            "(id_expansion INTEGER NOT NULL AUTO_INCREMENT,"+
-            "expansion_name VARCHAR(70) NOT NULL,"+
-            "PRIMARY KEY ( id_expansion ))";
-
-    String sqlArtists = "CREATE TABLE artists"+
-                                        "(id_artist INTEGER NOT NULL AUTO_INCREMENT,"+
-                                        "name VARCHAR(60) NOT NULL UNIQUE,"+
-                                        "PRIMARY KEY ( id_artist ))";
-
-    String sqlCardsExpansionConnection = "CREATE TABLE cards_expansion_connection"+
-            "(id_card INTEGER,"+ // FOREIGN KEY
-            "price DECIMAL(10,2),"+ //karty mogą mieć różne ceny w zalezności od dodatku
-            "id_expansion INTEGER)"; //FOREIGN KEY
-            //"FOREIGN KEY (id_card) REFERENCES cards(id_card),"+
-            //"FOREIGN KEY (id_expansion) REFERENCES expansions(id_epxanison))";
-
-    String sqlCardsArtistsConnection = "CREATE TABLE cards_artists_connection"+
-                                        "(id_card INTEGER," + //FOREIGN KEY
-                                        "id_artist INTEGER)"; //FOREIGN KEY
-                                        //"FOREIGN KEY (id_card) REFERENCES cards(id_card),"+
-                                        //"FOREIGN KEY (id_artist) REFERENCES artists(id_artist))";
+                CREATE TABLE expansions(
+                    id_expansion INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                    expansion_name VARCHAR(70) NOT NULL
+                );
 
 
-    String sqlCardsExpansionConnection_FK1 = "ALTER TABLE cards_expansion_connection ADD CONSTRAINT cards_expansion_connection_FK1 FOREIGN KEY (id_card) REFERENCES cards(id_card)";
-    String sqlCardsExpansionConnection_FK2 = "ALTER TABLE cards_expansion_connection ADD CONSTRAINT cards_expansion_connection_FK2 FOREIGN KEY (id_expansion) REFERENCES expansions(id_expansion)";
-
-    String sqlCardsArtistsConnection_FK1 = "ALTER TABLE cards_artists_connection ADD CONSTRAINT cards_artists_connection_FK1 FOREIGN KEY (id_card) REFERENCES cards(id_card)";
-    String getSqlCardsArtistsConnection_FK2 = "ALTER TABLE cards_artists_connection ADD CONSTRAINT cards_artists_connection_FK2 FOREIGN KEY (id_artist) REFERENCES artists(id_artist)";
-
-
-    String[] sqlStructure =  {sqlCards, sqlExpansion,sqlArtists,sqlCardsArtistsConnection,sqlCardsExpansionConnection,sqlCardsExpansionConnection_FK1,sqlCardsExpansionConnection_FK2,sqlCardsArtistsConnection_FK1,getSqlCardsArtistsConnection_FK2};
+                CREATE TABLE artists(
+                    id_artist INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                    name VARCHAR(60) NOT NULL UNIQUE
+                );
 
 
+                CREATE TABLE users(
+                    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                    login VARCHAR(255) NOT NULL UNIQUE,
+                    password VARCHAR(255) NOT NULL
+                );
+
+
+
+                CREATE TABLE cards_expansion_connection(
+                    id_card INTEGER,
+                    price DECIMAL(10,2),
+                    id_expansion INTEGER
+                );
+
+
+                CREATE TABLE cards_artists_connection(
+                    id_card INTEGER,
+                    id_artist INTEGER
+                );
+
+
+                ALTER TABLE cards_expansion_connection ADD CONSTRAINT cards_expansion_connection_FK1 FOREIGN KEY (id_card) REFERENCES cards(id_card);
+                ALTER TABLE cards_expansion_connection ADD CONSTRAINT cards_expansion_connection_FK2 FOREIGN KEY (id_expansion) REFERENCES expansions(id_expansion);
+                ALTER TABLE cards_artists_connection ADD CONSTRAINT cards_artists_connection_FK1 FOREIGN KEY (id_card) REFERENCES cards(id_card);
+                ALTER TABLE cards_artists_connection ADD CONSTRAINT cards_artists_connection_FK2 FOREIGN KEY (id_artist) REFERENCES artists(id_artist);
+
+
+                COMMIT;
+
+            """;
 }
